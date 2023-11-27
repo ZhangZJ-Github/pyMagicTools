a9a6a59d8cd63d0b35e9462de32b6cb033d70434
yyh <1500178078@qq.com>
2023-11-27 16:22:31 +0800

    log Please enter the commit message for your changes. Lines starting :wq
    :wqi

--- log_parser.py
+++ log_parser.py
@@ -7,14 +7,32 @@
 """
 用于解析Magic的log文件
 """
+import pandas as pd
 import typing
-
 import _base
+import enum
+import re
+from _logging import logger
+import time
+import typing
+from io import StringIO
+from typing import List
+
+import matplotlib
+import shapely.geometry
+
 
 
 class LOG:
     def __init__(self, filename):
+        t = time.time()
         self.filename = filename
+        self.text = ""
+        with open(self.filename, 'r') as f:
+            self.text = f.read()
+        t1 = time.time()
+        logger.info("将文件%s读入内存（未处理）耗时：%.2f" % (filename, t1 - t))
+        self.geom_structure = self.parse_object_materialtype()
         # TODO: 参考其他parser的__init__
 
     def parse_object_materialtype(self) -> typing.OrderedDict[str, _base.MaterialType]:
@@ -34,3 +52,30 @@ class LOG:
 
         """
         # TODO:
+        self.materialtype_table_start = r'===> Structure Generator 2d <==='
+        it = re.finditer(self.materialtype_table_start, self.text)
+        for match in it:
+            sta=match.span()[1]+1
+        self.materialtype_table_end = '> ================================================\n          ... Testing for Boundary type OUTGOING'
+        it = re.finditer(self.materialtype_table_end, self.text)
+        for match in it:
+            end = match.span()[0]
+        list = self.text[sta:end].split('\n')
+        for i in range(len(list)):
+            list[i] = list[i].split()
+        columns = list[0]
+        del list [0]
+        df = pd.DataFrame(list,columns = columns)
+        df = df.drop(df.shape[0]-1)
+        return df
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    filename = r"E:\11-24\2\template_20231127_072536_54.log"
+    log = LOG(filename)
